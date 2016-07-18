f42c4a9a4e26269a8cb91dd5eed7c2240984d92a
jccultima123 <johncyrillcorsanes@gmail.com>
2016-07-18 09:38:00 +0800

    [TEST] Error Handling

---
+++ views/error.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="login-panel panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><?php echo (isset($title) ? $title : 'ERROR!'); ?></h3>
+                </div>
+                <div class="panel-body">
+                    
+                    <p><?php echo $message; ?></p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
