49571d3c677271a713d59037b693c06afb1d4424
jccultima123 <johncyrillcorsanes@gmail.com>
2017-02-05 22:21:04 +0800

    Added more instructional comments in index

--- index.php
+++ index.php
@@ -7,21 +7,26 @@
  * @license http://opensource.org/licenses/MIT MIT License
  */
 
-// classes
-require_once("classes/Core.php"); // core components first such as main classes then load dependencies
+// core components first such as main classes then load dependencies
+require_once("classes/Core.php");
+// then Innstatiate the class to use it
 $core = new Core();
-
-// load the login class
+// load the login class then instantiate again
 require_once("classes/Auth.php");
-
 $auth = new Auth();
 
+// Now include the HEADER constant (based on your config.php)
 include(HEADER);
 
-if ($auth->isUserLoggedIn()) { // if user logged in
-	$core->render("views/logged_in.php");
+// Now put your own logic to render the page
+// (this is a sample then you can do another on your own)
+
+// Now, the instantiated class variables are going to be used
+if ($auth->isUserLoggedIn()) { // if user logged in (using Auth class)
+	$core->render("views/logged_in.php"); // use Core class to render
 } else { // not logged in
-	$core->render("views/not_logged_in.php", 'file');
+	$core->render("views/not_logged_in.php", 'file'); // do the fallback function
 }
 
+// Don't forget the FOOTER
 include(FOOTER);
