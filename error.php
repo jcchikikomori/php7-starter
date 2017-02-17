8fd3da7f6203faa580812beff884de4b59cd938e
jccultima123 <johncyrillcorsanes@gmail.com>
2017-02-17 12:04:38 +0800

    Better Error Handling

--- error.php
+++ error.php
@@ -1,34 +1,26 @@
 <?php
 
+require_once("classes/Core.php");
+$core = new Core();
+
 /**
  * @author Chris Coyier
  * @source https://css-tricks.com/snippets/php/error-page-to-handle-all-errors/
  */
-
-require_once("classes/Core.php");
-$core = new Core();
-
 $status = $_SERVER['REDIRECT_STATUS'];
-
 $codes = array(
-   403 => array('403 Forbidden', 'The server has refused to fulfill your request.'),
-   404 => array('404 Not Found', 'The document/file requested was not found on this server.'),
-   405 => array('405 Method Not Allowed', 'The method specified in the Request-Line is not allowed for the specified resource.'),
-   408 => array('408 Request Timeout', 'Your browser failed to send a request in the time allowed by the server.'),
-   500 => array('500 Internal Server Error', 'The request was unsuccessful due to an unexpected condition encountered by the server.'),
-   502 => array('502 Bad Gateway', 'The server received an invalid response from the upstream server while trying to fulfill the request.'),
-   504 => array('504 Gateway Timeout', 'The upstream server failed to send a request in the time allowed by the server.'),
+    403 => array('403 Forbidden', 'The server has refused to fulfill your request.'),
+    404 => array('404 Not Found', 'The document/file requested was not found on this server.'),
+    405 => array('405 Method Not Allowed', 'The method specified in the Request-Line is not allowed for the specified resource.'),
+    408 => array('408 Request Timeout', 'Your browser failed to send a request in the time allowed by the server.'),
+    500 => array('500 Internal Server Error', 'The request was unsuccessful due to an unexpected condition encountered by the server.'),
+    502 => array('502 Bad Gateway', 'The server received an invalid response from the upstream server while trying to fulfill the request.'),
+    504 => array('504 Gateway Timeout', 'The upstream server failed to send a request in the time allowed by the server.'),
 );
+$data['title'] = $codes[$status][0];
+$data['message'] = $codes[$status][1];
+//if ($data['title'] == false || strlen($status) != 3) {
+//    $data['message'] = 'Please supply a valid status code.';
+//}
 
-$title = $codes[$status][0];
-$message = $codes[$status][1];
-
-if ($title == false || strlen($status) != 3) {
-   $message = 'Please supply a valid status code.';
-}
-
-include(HEADER);
-
-include("views/error.php"); // unified error page
-
-include(FOOTER);
+$core->render("error.php", NULL, $data); // unified error page
--- views/error.php
+++ views/error.php
@@ -3,12 +3,19 @@
         <div class="col-md-4 col-md-offset-4">
             <div class="login-panel panel panel-default">
                 <div class="panel-heading">
-                    <h3 class="panel-title"><?php echo (isset($title) ? $title : 'ERROR!'); ?></h3>
+                    <h3 class="panel-title">
+                        <?php echo (isset($data['title']) ? $data['title'] : 'ERROR!'); ?>
+                    </h3>
                 </div>
                 <div class="panel-body">
-                    
-                    <p><?php echo $message; ?></p>
-
+                    <p>
+                        <?php echo (isset($data['message']) ? $data['message'] : "There's something wrong.."); ?>
+                    </p>
+                    <?php if (isset($data['debug'])) { ?>
+                        <code>
+                            <?php echo (isset($data['debug']) ? $data['debug'] : "Cannot find logs here."); ?>
+                        </code>
+                    <?php } ?>
                 </div>
             </div>
         </div>
