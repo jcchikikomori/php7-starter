3627b3391d5c96fddf82c7dfc8ea680e83f0033c
jccultima123 <johncyrillcorsanes@gmail.com>
2017-03-11 20:58:27 +0800

    Another files to handle configs. Yeah, separated

--- configs/system.php
+++ configs/system.php
@@ -10,22 +10,3 @@
  * Environment. Details is on classes/Init.php
  */
 define('ENVIRONMENT', 'development');
-
-/**
- * Configuration for: Database Connection
- *
- * DB_HOST: database host, usually it's "127.0.0.1" or "localhost", some servers also need port info
- * DB_NAME: name of the database. please note: database and database table are not the same thing
- * DB_USER: user for your database. the user needs to have rights for SELECT, UPDATE, DELETE and INSERT.
- * DB_PASS: the password of the above user
- * DB_PORT: database port
- * DB_TYPE: database type. mysql is default
- * DB_FILE: this is completely optional, unless you are gonna use SQLITE
- */
-define("DB_HOST", "127.0.0.1");
-define("DB_NAME", "myphp_db");
-define("DB_USER", "root");
-define("DB_PASS", "root");
-define("DB_PORT", 3306);
-define("DB_TYPE", 'mysql');
-define("DB_FILE", 'database.db'); // FOR SQLITE!!
