020c27b62060de39e1b6f7811832b27838c6fe0b
jccultima123 <johncyrillcorsanes@gmail.com>
2017-03-17 08:55:57 -0400

    Updating CA Script

--- ca-stack-setup.sh
+++ ca-stack-setup.sh
@@ -2,26 +2,30 @@
 
 # REQUIRES ELEVATED ACCESS!! Execute this in sudo mode
 
+# updating
+apt-get update
 # setup for incorrect charsets
 apt-get install -y software-properties-common python-software-properties language-pack-en-base
-LC_ALL=en_US.UTF-8
-# adding repositories
-add-apt-repository ppa:ondrej/php
+echo "Updating Composer"
+composer self-update
+LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-# updating
-echo "Updating repositories"
-apt-get update
-# install php 7
-echo "Installing PHP 7 CLI"
-apt-get install -y php7.0 php7.0-gd php7.0-mbstring php7.0-sqlite php7.0-mysql
 echo "Installing NodeJS"
 apt-get install -y nodejs
 echo "Installing Bower"
 npm install -g bower
+# install php 7
+echo "Installing PHP 7 CLI"
+apt-get install -y php7.0
+echo "Installing MyPHP"
+composer self-update
+bower install
 echo "Installing required softwares.."
-sudo apt-get install php libapache2-mod-php
-sudo a2dismod php5
-sudo a2enmod php7.0
+apt-get install -y php php7.0-gd php7.0-mbstring php7.0-sqlite php7.0-mysql libapache2-mod-php
+echo "Disabling PHP5 on server"
+a2dismod php5
+echo "Activating PHP7 on server"
+a2enmod php7.0
 # restarting server
 service apache2 restart
 # finishes
