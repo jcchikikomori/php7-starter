a911452b164f9af18e3c697ad5972f131ea87331
jccultima123 <johncyrillcorsanes@gmail.com>
2017-03-20 15:44:28 +0800

    Much better JSON response

--- libraries/JSON.php
+++ libraries/JSON.php
@@ -8,20 +8,15 @@ class JSON
     /**
      * Recommended REST values. You can change this if you wish
      * @param array $data - any of data or arrays you wish
-     * @param $status - response or status
      * @return string - JSON
      */
-    public static function encode($data=[], $status=[]) {
+    public static function encode($data=[]) {
         $array = [];
         if (!empty($data)) {
             // you can change this based on MyPHP class
             foreach($data as $dat => $key) {
-                $array['response'][$dat] = $key;
-            }
-        }
-        if (!empty($status)) {
-            foreach($status as $stat) {
-                $array['status'][] = $stat;
+                // $array['response'][$dat] = $key;
+                $array[$dat] = $key; // much better!
             }
         }
         return json_encode($array); // return as response
