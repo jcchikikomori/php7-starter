03a1a865767e49e588a310bfbac18e5c4cc560a8
jccultima123 <johncyrillcorsanes@gmail.com>
2017-04-16 19:22:34 +0800

    [UPDATE] OOPS

--- views/_templates/header.php
+++ views/_templates/header.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="description" content="">
-    <title>MyPHP Plus</title>
+    <title>MyPHP</title>
 
     <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="bower_components/startbootstrap-sb-admin-2/dist/css/sb-admin-2.css" rel="stylesheet">
